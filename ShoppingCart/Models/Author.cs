3024641ad2e33497861709be0eec9fc5a08d1271
KamilSobien <kamil.sobien@gmail.com>
2018-05-14 22:40:43 +0200

    Models, EntityFramework update

---
+++ ShoppingCart/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCart.Models {
+    public class Author {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        [NotMapped]
+        public string FullName {
+            get => FirstName + ' ' + LastName;
+        }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
