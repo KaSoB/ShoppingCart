b60d4914dc39ac0e7e3ef9bf0eae516a91170aef
KamilSobien <kamil.sobien@gmail.com>
2018-05-14 23:44:38 +0200

    CartService, CartsController, SummaryView

---
+++ ShoppingCart/Services/CartService.cs
@@ -0,0 +1,36 @@
+﻿using ShoppingCart.DAL;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingCart.Services {
+    public class CartService : IDisposable {
+        private ShoppingCartContext _db = new ShoppingCartContext();
+        public Cart GetBySessionId(string sessionId) {
+            var cart = _db
+                .Carts
+                .Include("CartItems")
+                .Where(c => c.SessionId == sessionId)
+                .SingleOrDefault();
+            cart = CreateCartIfItDoesntExist(sessionId, cart);
+            return cart;
+        }
+
+        private Cart CreateCartIfItDoesntExist(string sessionId, Cart cart) {
+            if (cart == null) {
+                cart = new Cart {
+                    SessionId = sessionId,
+                    CartItems = new List<CartItem>()
+                };
+                _db.Carts.Add(cart);
+                _db.SaveChanges();
+            }
+            return cart;
+        }
+
+        public void Dispose() {
+            _db.Dispose();
+        }
+    }
+}
