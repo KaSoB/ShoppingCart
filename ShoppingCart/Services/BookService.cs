31bd92f627c562ce4e9876a488f674bfba902f9d
KamilSobien <kamil.sobien@gmail.com>
2018-05-15 11:11:48 +0200

    BooksController, BookService, Views

---
+++ ShoppingCart/Services/BookService.cs
@@ -0,0 +1,29 @@
+﻿using ShoppingCart.DAL;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingCart.Services {
+    public class BookService : IDisposable {
+        private ShoppingCartContext _db = new ShoppingCartContext();
+
+        public List<Book> GetFeatured() {
+            return _db.Books
+                .Include("Author")
+                .Where(b => b.Featured)
+                .ToList();
+        }
+        public List<Book> GetByCategoryId(int categoryId) {
+            return _db.Books
+                .Include("Author")
+                .Where(b => b.CategoryId == categoryId)
+                .OrderByDescending(b => b.Featured)
+                .ToList();
+        }
+
+        public void Dispose() {
+            _db.Dispose();
+        }
+    }
+}
